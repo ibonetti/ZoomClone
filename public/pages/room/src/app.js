bafad3529afd014483befaeda6c549a57d26f9c0
ibonetti <israelbonetti@gmail.com>
2021-02-03 16:33:06 -0200

    Peer, Recorder, More Participants and other stuff

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -1,13 +1,4 @@
 
-
-const recordClick = function (recorderBtn) {
-  this.recordingEnabled = false
-  return () => {
-    this.recordingEnabled = !this.recordingEnabled
-    recorderBtn.style.color = this.recordingEnabled ? 'red' : 'white'
-  }
-}
-
 const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const room = urlParams.get('room');
@@ -18,13 +9,25 @@ const onload = () => {
 
   const socketUrl = 'http://localhost:3000';
   const socketBuilder = new SocketBuilder({ socketUrl });
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/',
+    }
+  });
+
+  const peerBuilder = new PeerBuilder({ peerConfig });
   const view = new View();
   const media = new Media();
   const deps = {
     view,
     media,
     room,
-    socketBuilder
+    socketBuilder,
+    peerBuilder
   };
 
   Business.initialize(deps);
